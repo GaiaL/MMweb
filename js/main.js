85a79687f12239bfd46fd54eca3a977b42c4b613
Greeny Liu <rainy7843@gmail.com>
2015-11-08 02:04:00 -0800

    0.0

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$.backstretch([
+      "img/bg1.jpg",
+      "img/bg2.jpg",
+      "img/bg3.jpg",
+      "img/bg4.jpg",
+      "img/bg5.jpg",
+      "img/bg6.jpg"
+      ], {
+        fade: 750,
+        duration: 4000
+    });
